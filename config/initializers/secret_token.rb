c07a2764925ec10f8f1101f521242c5752e74d93
think <think@www6319uj.sakura.ne.jp>
2013-04-30 12:17:03 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authonality::Application.config.secret_token = 'b233e3e13589128d6a122f63d1e6532584073f616bfdfeabeb507349c3b8b143f5f746f9a97fd8c61a997189e0a1ed37601f806fab801f9a4a1f896018e5cee8'
